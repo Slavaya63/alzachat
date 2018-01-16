68a50f62ba4461983d2f509bb61c541855fa1de3
Svyatoslav Titov <stitov@axbit.ru>
2018-01-16 17:48:39 +0400

    Init

---
+++ Service/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Service.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task Send(string message)
+        {
+            return Clients.All.InvokeAsync("Send", message);
+        }
+    }
+}
